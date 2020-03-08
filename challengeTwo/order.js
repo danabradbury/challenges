58a5d14fb8fbb014a1e3784849ad96b7537b6a5e
Dmarcus Spencer <spencerdd@myips.org>
2020-03-08 10:08:23 -0400

    - added eslint

--- challengeTwo/order.js
+++ challengeTwo/order.js
@@ -2,44 +2,29 @@
 //  I don't care what the new order is, as long as it chnages, 
 // and the html is still "good", meaning you still have a UL tag enclosing 3 LI tags (inner html!)
 
+function randomItem() {
 
-/* grabbed and set values */
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+    let max = 3;
+    let randomMax = randomNumber * max;
+    console.log(randomMax);
+    let retVal = Math.floor(randomMax);
+    console.log(retVal);
 
-
-function hitIt() {
-
-
-    let itemsOne = document.getElementById('itemOne').innerHTML
-    console.log(itemsOne)
-
-    let itemTwo = document.getElementById('itemTwo').innerHTML
-    console.log(itemTwo)
-
-    let itemThree = document.getElementById('itemThree').innerHTML
-    console.log(itemThree)
-
-    /* combinaed all items */
-    let items =[itemsOne, itemTwo, itemThree]
-    console.log(items)
-
-    /* grabbed spacific item */
-    let grabItem = items.length
-    console.log(grabItem)
-
-
-    takedown = numberToChoice(randomItem());
-    console.log('dana', takedown)
-
-    function numberToChoice(grabItem) {
-        return [grabItem];
-    }
-
-    function randomItem() {
-        return Math.floor(Math.random() * 3);
-
-    }
-
-
-    
+    return retVal;
 }
 
+function hitIt() {
+    const itemOne = document.getElementById('itemOne');
+    const itemTwo = document.getElementById('itemTwo');
+    const itemThree = document.getElementById('itemThree');
+
+    const items = [itemOne, itemTwo, itemThree];
+
+    const takedown = items[randomItem()];
+    console.log(takedown);
+
+    const unOrderedList = document.getElementById('justAnUnorderedList');
+    unOrderedList.appendChild(takedown);
+}
