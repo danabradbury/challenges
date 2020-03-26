3ffc16e8c774de53339061a57ee77bd7e9793174
Dmarcus Spencer <spencerdd@myips.org>
2020-03-26 16:39:05 -0400

    updated

--- challengeThree/scripts.js
+++ challengeThree/scripts.js
@@ -1,1 +1,13 @@
 //write some functions here to move the values of the inouts to new <LI> elements in the 'outputs' <UL>
+
+function list() {
+    const input1 = document.getElementById('box1');
+    Console.log(input1);
+
+    const input2 = document.getElementById('box2');
+    Console.log(input2);
+
+    const input3 = document.getElementById('box3');
+    Console.log(input3);
+
+}
